732765ae30d29ecd01bad179b62563987b353560
CC <dsy1998@live.com>
2016-03-31 23:15:31 -1000

    Remove another unnecessary input.

--- src/org/usfirst/frc/team6179/robot/Extreme3DPro.java
+++ src/org/usfirst/frc/team6179/robot/Extreme3DPro.java
@@ -51,11 +51,6 @@ public class Extreme3DPro implements OI {
     }
 
     @Override
-    public double getShooterTriggerServoMovement() {
-        return 0;
-    }
-
-    @Override
     public double getShooterElevatorInput() {
         return 0;
     }
